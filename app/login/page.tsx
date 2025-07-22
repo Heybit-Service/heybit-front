1d3967ed2aebc29b00e114f25cada1ac5511c859
doYoungAn <jispoop@gmail.com>
2025-07-22 21:57:37 +0900

    로그인 페이지 생성

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import { FC } from 'react';
+import { Wrapper, Title, KaKaoButton, GoogleButton, Footer } from './styles';
+import Logo from '@/assets/logo.svg';
+import CharacterDefault from '@/assets/character/character-default.svg';
+import KakaoIcon from '@/assets/login/kakao.svg';
+import GoogleIcon from '@/assets/login/google.svg';
 
 const LoginPage: FC = () => {
     return (
         <>
-            <div>LoginPage</div>
+            <Wrapper>
+                <Logo style={{ marginBottom: '16px' }} />
+                <Title>Hey! 잠깐 멈춰볼까요?</Title>
+                <CharacterDefault style={{ marginBottom: '60px' }} />
+                <KaKaoButton>
+                    <KakaoIcon />
+                    카카오 계정으로 로그인
+                </KaKaoButton>
+                <GoogleButton>
+                    <GoogleIcon />
+                    구글 계정으로 로그인
+                </GoogleButton>
+                <Footer>
+                    계속하면 <span style={{ textDecoration: 'underline' }}>서비스이용약관</span>과 <span style={{ textDecoration: 'underline' }}>개인정보처리방침</span>에 <br/>동의하게 됩니다
+                </Footer>
+            </Wrapper>
         </>
     )
 }
---
+++ constant/color.ts
@@ -0,0 +1,9 @@
+export const GRAY = {
+    100: '#F7F7F7',
+    200: '#E8E8E8',
+    300: '#A8A8A8',
+    400: '#7C7C7C',
+    500: '#5B5B5B',
+}
+
+export const BLACK = '#202020';
