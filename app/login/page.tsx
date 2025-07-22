ac3b5c5d7ea5815fa54f39224a3d1a510fc1789b
doYoungAn <jispoop@gmail.com>
2025-07-22 22:10:22 +0900

    페이지 이동 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FC } from 'react';
+import { useRouter } from 'next/navigation';
 import { Wrapper, Title, KaKaoButton, GoogleButton, Footer } from './styles';
 import Logo from '@/assets/logo.svg';
 import CharacterDefault from '@/assets/character/character-default.svg';
@@ -8,17 +9,32 @@ import KakaoIcon from '@/assets/login/kakao.svg';
 import GoogleIcon from '@/assets/login/google.svg';
 
 const LoginPage: FC = () => {
+
+    const router = useRouter();
+
+    const handleKakaoLogin = () => {
+        // Implement Kakao login logic here
+        // console.log("Kakao login clicked");
+        router.push('/register'); // Redirect to register page after login
+    }; 
+
+    const handleGoogleLogin = () => {
+        // Implement Google login logic here
+        // console.log("Google login clicked");
+        router.push('/register'); // Redirect to register page after login
+    };
+    
     return (
         <>
             <Wrapper>
                 <Logo style={{ marginBottom: '16px' }} />
                 <Title>Hey! 잠깐 멈춰볼까요?</Title>
                 <CharacterDefault style={{ marginBottom: '60px' }} />
-                <KaKaoButton>
+                <KaKaoButton onClick={handleKakaoLogin}>
                     <KakaoIcon />
                     카카오 계정으로 로그인
                 </KaKaoButton>
-                <GoogleButton>
+                <GoogleButton onClick={handleGoogleLogin}>
                     <GoogleIcon />
                     구글 계정으로 로그인
                 </GoogleButton>
