9f4f49830638d672d9986c767ceb90150c2b4839
relkimm <relkimm@grandeclip.com>
2025-08-09 22:26:16 +0900

    feat: 타이머 시작 화면 구현

--- app/timer/start/page.tsx
+++ app/timer/start/page.tsx
@@ -1,10 +1,28 @@
 import { AppBar } from '@/components/app-bar';
 import { BackButton } from '@/components/button/back-button';
+import { SizedBox } from '@/components/sized-box';
+import Bubble from '@/assets/timer/start/bubble.png';
+import Character from '@/assets/timer/start/character.png';
+import TimePicker from '@/assets/timer/start/time_picker.png';
+import Image from 'next/image';
+import { TimerStartButton } from '@/components/timer/button/timer-start';
 
 export default function Page() {
   return (
-    <div>
+    <div className="h-screen bg-[#F7F7F7]">
       <AppBar title="타이머 시작" leadings={<BackButton />} />
+      <SizedBox className="h-16" />
+      <div className="flex flex-col items-center gap-8">
+        <div>
+          <Image className="px-9" src={Bubble} alt="bubble" />
+          <SizedBox className="h-6" />
+          <Image className="px-28" src={Character} alt="character" />
+        </div>
+        <Image src={TimePicker} alt="time-picker" />
+      </div>
+      <div className="absolute bottom-14 w-full px-4">
+        <TimerStartButton />
+      </div>
     </div>
   );
 }
---
+++ components/timer/button/timer-start.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export const TimerStartButton = () => {
+  return (
+    <Link
+      href={`/timer/start/step1`}
+      className="block w-full py-4 bg-[#0EC189] font-pretendard font-semibold text-lg leading-[150%] text-center text-white rounded-[10px]"
+    >
+      타이머 시작
+    </Link>
+  );
+};
