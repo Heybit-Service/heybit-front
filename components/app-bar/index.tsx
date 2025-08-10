5627540202402472918a7bf2fccee039a17d91e3
relkimm <relkimm@grandeclip.com>
2025-08-10 16:52:30 +0900

    feat: 앱바 bg color > white 로 변경

--- components/app-bar/index.tsx
+++ components/app-bar/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const AppBar = ({ title, leadings, actions }: Props) => {
   return (
-    <div className="h-15 flex items-center relative">
+    <div className="h-15 flex items-center relative bg-white">
       <div className="flex items-center gap-2">{leadings}</div>
       <h1 className="absolute left-1/2 transform -translate-x-1/2 text-xl font-semibold text-center leading-[133%] tracking-normal">
         {title}
