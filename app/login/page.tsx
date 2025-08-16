7cf83f1860e00121399af3e993be77d12ddc2ae0
Yoo Seung Su <zenorate@gmail.com>
2025-08-16 23:55:33 +0900

    feat: 로그인 페이지 약관 링크 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { FC } from 'react';
 import { useRouter } from 'next/navigation';
@@ -9,41 +9,59 @@ import KakaoIcon from '@/assets/login/kakao.svg';
 import GoogleIcon from '@/assets/login/google.svg';
 
 const LoginPage: FC = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const handleKakaoLogin = () => {
+    // Implement Kakao login logic here
+    // console.log("Kakao login clicked");
+    router.push('/on-boarding'); // Redirect to onboarding page after login
+  };
 
-    const handleKakaoLogin = () => {
-        // Implement Kakao login logic here
-        // console.log("Kakao login clicked");
-        router.push('/on-boarding'); // Redirect to onboarding page after login
-    }; 
+  const handleGoogleLogin = () => {
+    // Implement Google login logic here
+    // console.log("Google login clicked");
+    router.push('/on-boarding'); // Redirect to onboarding page after login
+  };
 
-    const handleGoogleLogin = () => {
-        // Implement Google login logic here
-        // console.log("Google login clicked");
-        router.push('/on-boarding'); // Redirect to onboarding page after login
-    };
-    
-    return (
-        <>
-            <Wrapper>
-                <Logo style={{ marginBottom: '16px' }} />
-                <Title>Hey! 잠깐 멈춰볼까요?</Title>
-                <CharacterDefault style={{ marginBottom: '60px' }} />
-                <KaKaoButton onClick={handleKakaoLogin}>
-                    <KakaoIcon />
-                    카카오 계정으로 로그인
-                </KaKaoButton>
-                <GoogleButton onClick={handleGoogleLogin}>
-                    <GoogleIcon />
-                    구글 계정으로 로그인
-                </GoogleButton>
-                <Footer>
-                    계속하면 <span style={{ textDecoration: 'underline' }}>서비스이용약관</span>과 <span style={{ textDecoration: 'underline' }}>개인정보처리방침</span>에 <br/>동의하게 됩니다
-                </Footer>
-            </Wrapper>
-        </>
-    )
-}
+  return (
+    <>
+      <Wrapper>
+        <Logo style={{ marginBottom: '16px' }} />
+        <Title>Hey! 잠깐 멈춰볼까요?</Title>
+        <CharacterDefault style={{ marginBottom: '60px' }} />
+        <KaKaoButton onClick={handleKakaoLogin}>
+          <KakaoIcon />
+          카카오 계정으로 로그인
+        </KaKaoButton>
+        <GoogleButton onClick={handleGoogleLogin}>
+          <GoogleIcon />
+          구글 계정으로 로그인
+        </GoogleButton>
+        <Footer>
+          계속하면{' '}
+          <a
+            href="https://www.notion.so/230b5c7320fe8058b6c4e07aa2f2fcc3?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'underline', color: 'inherit' }}
+          >
+            서비스 이용약관
+          </a>
+          과{' '}
+          <a
+            href="https://www.notion.so/230b5c7320fe8015a782e58d174f594b?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'underline', color: 'inherit' }}
+          >
+            개인정보처리방침
+          </a>
+          에 <br />
+          동의하게 됩니다
+        </Footer>
+      </Wrapper>
+    </>
+  );
+};
 
 export default LoginPage;
