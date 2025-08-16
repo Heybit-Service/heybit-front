9bf22a90e843db96ca951aef0876ca55679ee87c
Yoo Seung Su <zenorate@gmail.com>
2025-08-17 02:48:33 +0900

    fix: EmptyLayout 모바일 뷰포트 높이 동적 적용

--- components/layout/emptyLayout/styles.tsx
+++ components/layout/emptyLayout/styles.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import styled from 'styled-components';
-import { GRAY, BLACK } from '@/constant/color';
+import { GRAY } from '@/constant/color';
 
 export const Wrapper = styled.div`
   background-color: ${GRAY[100]};
   width: 100%;
   height: 100vh;
+  @supports (height: 100dvh) {
+    height: 100dvh;
+  }
   padding: 0 16px;
   display: flex;
   flex-direction: column;
