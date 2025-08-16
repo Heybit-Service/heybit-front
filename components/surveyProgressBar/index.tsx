e79e0f3540e99f9c60473b1db061c2ad9f838fb7
Yoo Seung Su <zenorate@gmail.com>
2025-08-17 00:28:29 +0900

    feat: 설문조사 프로그레스바 컴포넌트 생성

---
+++ components/surveyProgressBar/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Props {
+  progressbar:
+    | "progress-01"
+    | "default"
+    | "progress-02"
+    | "progress-sucess"
+    | "progress-03";
+}
+
+export const SurveyProgressBar = ({ progressbar }: Props): JSX.Element => {
+  return (
+    <div className="w-[calc(100%+32px)] h-1 bg-heybit-variable-HB-gray200 -ml-4">
+      {[
+        "progress-01",
+        "progress-02",
+        "progress-03",
+        "progress-sucess",
+      ].includes(progressbar) && (
+        <div
+          className={`h-1 bg-heybit-variable-HB-green-main ${progressbar === "progress-01" ? "w-1/4" : (progressbar === "progress-02") ? "w-1/2" : progressbar === "progress-03" ? "w-3/4" : "w-full"} ${["progress-01", "progress-02", "progress-03"].includes(progressbar) ? "rounded-[0px_10px_10px_0px]" : ""}`}
+        />
+      )}
+    </div>
+  );
+};
