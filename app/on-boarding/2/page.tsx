fa94abc8b2bb00be3eb2634df5c2a7a761c83efe
Yoo Seung Su <zenorate@gmail.com>
2025-08-16 21:28:43 +0900

    feat: 온보딩 2페이지에 명시적 스킵 경로 추가

--- app/on-boarding/2/page.tsx
+++ app/on-boarding/2/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import OnboardingLayout from '@/components/onboarding/OnboardingLayout';
+import ImageWithSkeleton from '@/components/onboarding/ImageWithSkeleton';
+import OnboardingImage2 from '@/assets/on-boarding/2.webp';
 
 export default function OnboardingPage2() {
     return (
         <OnboardingLayout
             currentPage={2}
             nextPath="/on-boarding/3"
-            title="온보딩 2페이지"
+            skipPath="/register"
+            title={<>살까 말까?<br />고민을 공유하고 의견을 나눠요</>}
         >
-            <div>
-                {/* 온보딩 2페이지 이미지 */}
-            </div>
+            <ImageWithSkeleton 
+                src={OnboardingImage2} 
+                alt="온보딩 이미지 2"
+                priority={true}
+                quality={100}
+                style={{ width: '100%', height: 'auto' }}
+                skeletonHeight="400px"
+            />
         </OnboardingLayout>
     );
 }
