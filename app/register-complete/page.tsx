7930d4d083000992cb64c0fc77e87f56dd500e06
Yoo Seung Su <zenorate@gmail.com>
2025-08-17 00:53:22 +0900

    style: 회원가입 완료 페이지 버튼 위치 조정

--- app/register-complete/page.tsx
+++ app/register-complete/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useRouter } from 'next/navigation';
 import { Wrapper, Title, Description } from './styles';
@@ -6,26 +6,36 @@ import Character from '@/assets/register-complete/character.svg';
 import FullButton from '@/components/button/fullButton';
 
 export default function RegisterPage() {
+  const router = useRouter();
 
-    const router = useRouter();
-    
-    const handleStart = () => {
-        router.push('/dashboard/timer/progress'); // Redirect to the survey page after onboarding
-    }
+  const handleStart = () => {
+    router.push('/dashboard/timer/progress');
+  };
 
-    return (
-        <Wrapper>
-            <Character style={{ marginTop: '110px', marginBottom: '31px' }} />
-            <Title style={{ marginBottom: '16px' }}>
-                무의식적인 소비, <br />
-                한 번 돌아보셨나요?
-            </Title>
-            <Description>
-                헤이빗과 함께 충동적인 소비를 멈추고<br />
-                경험을 나누며 건강한 소비 습관을 만들어요
-            </Description>
+  return (
+    <Wrapper>
+      <Character style={{ marginTop: '110px', marginBottom: '31px' }} />
+      <Title style={{ marginBottom: '16px' }}>
+        무의식적인 소비, <br />한 번 돌아보셨나요?
+      </Title>
+      <Description>
+        헤이빗과 함께 충동적인 소비를 멈추고
+        <br />
+        경험을 나누며 건강한 소비 습관을 만들어요
+      </Description>
 
-            <FullButton style={{ marginTop: 'auto', marginBottom: '56px' }} onClick={handleStart}>소비관리 시작</FullButton>
-        </Wrapper>
-    )
+      <FullButton
+        style={{
+          marginTop: '10px',
+          position: 'absolute',
+          bottom: '56px',
+          left: '16px',
+          width: 'calc(100% - 32px)',
+        }}
+        onClick={handleStart}
+      >
+        소비관리 시작
+      </FullButton>
+    </Wrapper>
+  );
 }
