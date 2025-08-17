05a9c9756afa3a69ec9c683a046218daef1e90d7
Yoo Seung Su <zenorate@gmail.com>
2025-08-18 01:13:08 +0900

    feat: FAB 컴포넌트 위치 조정 기능 추가

--- components/fab/index.tsx
+++ components/fab/index.tsx
@@ -3,7 +3,11 @@
 import PlusIcon from '@/assets/icon/plus.svg';
 import { useRouter } from 'next/navigation';
 
-export const Fab = () => {
+interface FabProps {
+  bottom?: string;
+}
+
+export const Fab = ({ bottom = '100px' }: FabProps = {}) => {
   const router = useRouter();
 
   const onClick = () => {
@@ -12,7 +16,8 @@ export const Fab = () => {
 
   return (
     <button
-      className="fixed bottom-25 right-4 w-16 h-16 bg-[#202020] rounded-full flex items-center justify-center"
+      className={`fixed right-4 w-16 h-16 bg-[#202020] rounded-full flex items-center justify-center z-50 md:right-[calc(50%-200px)]`}
+      style={{ bottom }}
       onClick={onClick}
     >
       <PlusIcon />
