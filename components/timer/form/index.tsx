3f59b965f56fed49daffc14689f94fb0dcbd3bfc
relkimm <relkimm@grandeclip.com>
2025-08-17 17:47:22 +0900

    feat: 타이머 등록 버튼 fixed 로 변경

--- components/timer/form/index.tsx
+++ components/timer/form/index.tsx
@@ -17,8 +17,8 @@ interface Props {
 export const TimerForm = ({ onSubmit }: Props) => {
   const [form, setForm] = useAtom(formAtom);
   return (
-    <div className="flex flex-col gap-13 px-4">
-      <div className="flex flex-col gap-6">
+    <div className="h-dvh flex flex-col items-center gap-13 px-4">
+      <div className="flex flex-col gap-6 pb-[126px]">
         <Title />
         <Category onChange={(value) => setForm({ ...form, category: value })} />
         <Name onChange={(value) => setForm({ ...form, name: value })} />
@@ -27,12 +27,14 @@ export const TimerForm = ({ onSubmit }: Props) => {
         <Upload onChange={(value) => setForm({ ...form, image: value })} />
         <Voting onChange={(value) => setForm({ ...form, voting: value })} />
       </div>
-      <button
-        className="w-full bg-[#202020] py-4 font-pretendard font-semibold text-xl text-[#FFFFFF] leading-[150%] text-center rounded-[10px]"
-        onClick={() => onSubmit(form)}
-      >
-        등록
-      </button>
+      <div className="w-full fixed bottom-0 px-4 pt-[10px] pb-14 bg-[#F7F7F7]">
+        <button
+          className="w-full bg-[#202020] py-4 font-semibold text-xl text-[#FFFFFF] leading-[150%] text-center rounded-[10px]"
+          onClick={() => onSubmit(form)}
+        >
+          등록
+        </button>
+      </div>
     </div>
   );
 };
