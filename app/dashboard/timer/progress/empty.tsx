01978d3df296d15affd38937d00f8312c4c9db9a
relkimm <relkimm@grandeclip.com>
2025-08-17 15:29:01 +0900

    fix: timer 대시보드 버튼 css 수정

--- app/dashboard/timer/progress/empty.tsx
+++ app/dashboard/timer/progress/empty.tsx
@@ -6,9 +6,9 @@ import TimerCount from '@/components/timer/count';
 
 export const Empty = () => {
   return (
-    <div className="flex flex-col items-center h-full pt-30">
+    <div className="flex flex-col items-center h-full pt-30 gap-22">
       <Image className="px-18" src={EmptyCharacter} alt="progress-empty-character" />
-      <div className="flex flex-col gap-3 w-full mt-auto">
+      <div className="flex flex-col gap-3 w-full">
         <TimerCount count={0} />
         <Link
           href="/timer/start"
