076c8b8df8a9029d27629ef004d2cadf69c4e319
Yoo Seung Su <zenorate@gmail.com>
2025-08-18 01:12:40 +0900

    feat: 투표 페이지 레이아웃 스크롤 개선

--- app/dashboard/vote/layout.tsx
+++ app/dashboard/vote/layout.tsx
@@ -1,26 +1,29 @@
 "use client";
 
-import { FC, PropsWithChildren } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { TabWrapper } from './styles';
+import { VoteLayoutWrapper, TabWrapper, ContentWrapper } from './styles';
+import { Fab } from '@/components/fab';
 
-const VoteLayout: FC<PropsWithChildren> = ({ children }) => {
+export default function VoteLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
     
     return (
-        <> 
-            <TabWrapper>
-                <Link className={`item${pathname.startsWith('/dashboard/vote/progress') ? ' active' : ''}`} href={"/dashboard/vote/progress"}>
-                    진행중인 투표
-                </Link>
-                <Link className={`item${pathname.startsWith('/dashboard/vote/my') ? ' active' : ''}`} href={"/dashboard/vote/my"}>
-                    내투표
-                </Link>
-            </TabWrapper>  
-            {children}
+        <>
+            <VoteLayoutWrapper>
+                <TabWrapper>
+                    <Link className={`item${pathname.startsWith('/dashboard/vote/progress') ? ' active' : ''}`} href={"/dashboard/vote/progress"}>
+                        진행중인 투표
+                    </Link>
+                    <Link className={`item${pathname.startsWith('/dashboard/vote/my') ? ' active' : ''}`} href={"/dashboard/vote/my"}>
+                        내투표
+                    </Link>
+                </TabWrapper>
+                <ContentWrapper>
+                    {children}
+                </ContentWrapper>
+            </VoteLayoutWrapper>
+            <Fab bottom="100px" />
         </>
     )
 }
-
-export default VoteLayout;
