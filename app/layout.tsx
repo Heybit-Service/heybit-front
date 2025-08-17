b51e0e28a6aa70d9c35015bc857719ef3d9ea1e3
relkimm <relkimm@grandeclip.com>
2025-08-17 21:00:31 +0900

    feat: 타이머 시작 > wheel ui 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import StyledComponentsRegistry from '@/lib/registry';
-import '@ncdai/react-wheel-picker/style.css';
 import './globals.css';
 
 const geistSans = Geist({
@@ -16,7 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Heybit',
-  description: '헤이빗과 함께 유예 타이머와 살까말까 투표, 소비 리포트를 통해 불필요한 충동구매를 절제해 보세요.',
+  description:
+    '헤이빗과 함께 유예 타이머와 살까말까 투표, 소비 리포트를 통해 불필요한 충동구매를 절제해 보세요.',
   icons: {
     icon: '/icon.svg',
   },
@@ -32,9 +32,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <StyledComponentsRegistry>
           <div className="min-h-screen flex justify-center bg-gray-100">
-            <div className="w-full md:max-w-[430px] bg-white relative md:shadow-xl">
-              {children}
-            </div>
+            <div className="w-full md:max-w-[430px] bg-white relative md:shadow-xl">{children}</div>
           </div>
           <div id="popup-root" />
         </StyledComponentsRegistry>
--- components/timer/picker/index.tsx
+++ components/timer/picker/index.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import { WheelPicker, WheelPickerWrapper } from '@ncdai/react-wheel-picker';
+import Picker from 'react-mobile-picker';
+import './styles.css';
 
 interface Duration {
   day: number;
@@ -13,57 +14,82 @@ interface Props {
   onChanged: (duration: Duration) => void;
 }
 
-const DurationPicker = ({ onChanged: onChanged }: Props) => {
-  const [day, setDay] = useState(0);
-  const [hour, setHour] = useState(0);
-  const [minute, setMinute] = useState(0);
+type PickerValue = {
+  day: string;
+  hour: string;
+  minute: string;
+};
 
-  const dayOptions = Array.from({ length: 30 }, (_, i) => ({
-    label: i.toString(),
-    value: i.toString(),
-  }));
+const selections = {
+  day: Array.from({ length: 30 }, (_, i) => i.toString()),
+  hour: Array.from({ length: 24 }, (_, i) => i.toString()),
+  minute: Array.from({ length: 60 }, (_, i) => i.toString()),
+};
 
-  const hourOptions = Array.from({ length: 24 }, (_, i) => ({
-    label: i.toString(),
-    value: i.toString(),
-  }));
+const DurationPicker = ({ onChanged }: Props) => {
+  const [pickerValue, setPickerValue] = useState<PickerValue>({
+    day: '0',
+    hour: '0',
+    minute: '0',
+  });
 
-  const minuteOptions = Array.from({ length: 60 }, (_, i) => ({
-    label: i.toString(),
-    value: i.toString(),
-  }));
-
-  const handleChange = (duration: Duration) => {
-    setDay(duration.day);
-    setHour(duration.hour);
-    setMinute(duration.minute);
-    onChanged(duration);
+  const handleChange = (value: PickerValue) => {
+    const numericDuration: Duration = {
+      day: Number(value.day),
+      hour: Number(value.hour),
+      minute: Number(value.minute),
+    };
+    setPickerValue(value);
+    onChanged(numericDuration);
   };
 
   return (
-    <WheelPickerWrapper className="flex px-18 gap-7">
-      <WheelPicker
-        classNames={{}}
-        options={dayOptions}
-        value={day.toString()}
-        onValueChange={(value) => handleChange({ day: Number(value), hour, minute })}
-        optionItemHeight={20}
-      />
-      <WheelPicker
-        classNames={{}}
-        options={hourOptions}
-        value={hour.toString()}
-        onValueChange={(value) => handleChange({ day, hour: Number(value), minute })}
-        optionItemHeight={20}
-      />
-      <WheelPicker
-        classNames={{}}
-        options={minuteOptions}
-        value={minute.toString()}
-        onValueChange={(value) => handleChange({ day, hour, minute: Number(value) })}
-        optionItemHeight={20}
-      />
-    </WheelPickerWrapper>
+    <div className="w-full px-13">
+      <Picker
+        value={pickerValue}
+        onChange={handleChange}
+        wheelMode="natural"
+        height={220}
+        itemHeight={60}
+        style={{
+          display: 'flex',
+          gap: '30px',
+        }}
+      >
+        {Object.keys(selections).map((name) => (
+          <Picker.Column key={name} name={name}>
+            {selections[name as keyof typeof selections].map((option) => (
+              <Picker.Item
+                key={option}
+                value={option}
+                style={{
+                  width: '62px',
+                  borderBottom: '1.8px solid #EAEAEA',
+                  marginRight: 'auto',
+                  marginLeft: 'auto',
+                }}
+              >
+                {({ selected }) => (
+                  <div
+                    className="font-medium text-[28px] leading-[150%] text-center"
+                    style={{
+                      color: selected ? '#202020' : '#0000004D',
+                    }}
+                  >
+                    {option}
+                  </div>
+                )}
+              </Picker.Item>
+            ))}
+          </Picker.Column>
+        ))}
+      </Picker>
+      <div className="w-full flex gap-[30px] font-['Pretendard'] font-semibold text-base text-[#878787] leading-[150%]">
+        <span className="w-full text-center">일</span>
+        <span className="w-full text-center">시간</span>
+        <span className="w-full text-center">분</span>
+      </div>
+    </div>
   );
 };
 
