f0d8c2fd54d442261fc7c1ca63eb6f84428fd7f8
relkimm <relkimm@grandeclip.com>
2025-08-18 00:03:11 +0900

    feat: 타이머 시작 > 레이아웃 변경

--- app/timer/start/page.tsx
+++ app/timer/start/page.tsx
@@ -17,11 +17,16 @@ export default function Page() {
     setForm({ ...form, day: duration.day, hour: duration.hour, minute: duration.minute });
   };
 
+  const enabled = () => {
+    if (form.day === 0 && form.hour === 0 && form.minute === 0) return false;
+    return true;
+  };
+
   return (
     <>
       <div className="h-dvh bg-[#F7F7F7] flex flex-col">
         <AppBar title="타이머 시작" leadings={<BackButton />} />
-        <div className="flex flex-col items-center pb-[126px]">
+        <div className="flex-1 flex flex-col items-center overflow-y-auto">
           <SizedBox className="h-16" />
           <Image width={321} height={62} src={Bubble} alt="bubble" />
           <SizedBox className="h-6" />
@@ -29,8 +34,8 @@ export default function Page() {
           <SizedBox className="h-8" />
           <DurationPicker onChanged={onChanged} />
         </div>
-        <div className="w-full fixed bottom-0 left-0 right-0 px-4 pt-[10px] pb-14 bg-[#F7F7F7]">
-          <TimerStartButton />
+        <div className="w-full px-4 pt-[10px] pb-14 bg-[#F7F7F7]">
+          <TimerStartButton disabled={!enabled()} />
         </div>
       </div>
     </>
--- components/timer/button/timer-start.tsx
+++ components/timer/button/timer-start.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link';
 
-export const TimerStartButton = () => {
+interface Props {
+  disabled: boolean;
+}
+
+export const TimerStartButton = ({ disabled }: Props) => {
+  const textColor = disabled ? 'text-[#5B5B5B]' : 'text-[#FFFFFF]';
+  const backgroundColor = disabled ? 'bg-[#E8E8E8]' : 'bg-[#0EC189]';
+
   return (
     <Link
       href={`/timer/start/step`}
-      className="block w-full py-4 bg-[#0EC189] font-pretendard font-semibold text-lg leading-[150%] text-center text-white rounded-[10px]"
+      className={`block w-full py-4 font-pretendard font-semibold text-lg leading-[150%] text-center rounded-[10px] transition-colors duration-300 ease-in-out ${textColor} ${backgroundColor}`}
     >
       타이머 시작
     </Link>
