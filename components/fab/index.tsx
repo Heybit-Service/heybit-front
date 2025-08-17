fffcc7d0f4ffe4b088cc626bf0ef2ac9f3fa13e9
relkimm <relkimm@grandeclip.com>
2025-08-17 14:46:19 +0900

    fix: compile 에러 수정

--- components/fab/index.tsx
+++ components/fab/index.tsx
@@ -1,28 +1,21 @@
 'use client';
 
 import PlusIcon from '@/assets/icon/plus.svg';
-import { useAtomValue } from 'jotai';
 import { useRouter } from 'next/navigation';
-import { fabAtom } from './store';
 
 export const Fab = () => {
   const router = useRouter();
-  const fab = useAtomValue(fabAtom);
 
   const onClick = () => {
     router.push('/timer/start');
   };
 
   return (
-    <>
-      {fab.visible && (
-        <button
-          className="fixed bottom-25 right-4 w-16 h-16 bg-[#202020] rounded-full flex items-center justify-center"
-          onClick={onClick}
-        >
-          <PlusIcon />
-        </button>
-      )}
-    </>
+    <button
+      className="fixed bottom-25 right-4 w-16 h-16 bg-[#202020] rounded-full flex items-center justify-center"
+      onClick={onClick}
+    >
+      <PlusIcon />
+    </button>
   );
 };
