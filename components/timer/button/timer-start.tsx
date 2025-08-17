446c7723fcbbbba42abed63a1704cc09e95d3171
relkimm <relkimm@grandeclip.com>
2025-08-18 00:05:27 +0900

    feat: 타이머 시작 > disabled 기능 구현

--- components/timer/button/timer-start.tsx
+++ components/timer/button/timer-start.tsx
@@ -7,11 +7,13 @@ interface Props {
 export const TimerStartButton = ({ disabled }: Props) => {
   const textColor = disabled ? 'text-[#5B5B5B]' : 'text-[#FFFFFF]';
   const backgroundColor = disabled ? 'bg-[#E8E8E8]' : 'bg-[#0EC189]';
+  const href = disabled ? '' : '/timer/start/step';
+  const cursor = disabled ? 'cursor-default' : 'cursor-pointer';
 
   return (
     <Link
-      href={`/timer/start/step`}
-      className={`block w-full py-4 font-pretendard font-semibold text-lg leading-[150%] text-center rounded-[10px] transition-colors duration-300 ease-in-out ${textColor} ${backgroundColor}`}
+      href={href}
+      className={`block w-full py-4 font-pretendard font-semibold text-lg leading-[150%] text-center rounded-[10px] transition-colors duration-300 ease-in-out ${textColor} ${backgroundColor} ${cursor}`}
     >
       타이머 시작
     </Link>
