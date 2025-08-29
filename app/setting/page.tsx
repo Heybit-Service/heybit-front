1fdd834cb180d0409fb3be2c9e3ff66bafe0339e
relkimm <relkimm@grandeclip.com>
2025-08-29 21:27:43 +0900

    feat: 로그아웃 기능 구현

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -3,13 +3,22 @@
 import { AppBar } from '@/components/app-bar';
 import { BackButton } from '@/components/button/back';
 import { SettingItem } from '@/components/setting/item';
+import { removeToken } from '@/data/auth';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
+  const router = useRouter();
+
+  const onLogout = () => {
+    removeToken();
+    router.push('/login');
+  };
+
   return (
     <div className="h-dvh">
       <AppBar title="설정" leadings={<BackButton />} />
       <div className="px-4 flex flex-col items-start">
-        <SettingItem label="로그아웃" onClick={() => {}} />
+        <SettingItem label="로그아웃" onClick={onLogout} />
         <SettingItem label="회원 탈퇴" onClick={() => {}} />
         <SettingItem label="서비스 이용약관" onClick={() => {}} />
         <SettingItem label="개인정보 처리방침" onClick={() => {}} />
