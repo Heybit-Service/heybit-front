745881725f7a3f449ae6070bd58e828ca07aa64d
relkimm <relkimm@grandeclip.com>
2025-09-20 18:46:50 +0900

    feat: 타이머 상세 화면 > 투표한 사람이 있는 경우에만 bubble 보이도록 개선

--- components/timer/information/voting.tsx
+++ components/timer/information/voting.tsx
@@ -7,11 +7,13 @@ interface Props {
 }
 
 export function Voting({ timer }: Props) {
+  const totalVotes = timer.buyCount + timer.holdCount;
+
   return (
     <div className="flex flex-col gap-9 px-4">
       <span className="font-pretendard font-bold text-base leading-[150%]">살까말까 투표 결과</span>
       <div className="flex flex-col gap-2">
-        <VotingBubble buyCount={timer.buyCount} stopCount={timer.holdCount} />
+        {totalVotes > 0 && <VotingBubble buyCount={timer.buyCount} stopCount={timer.holdCount} />}
         <VotingBar buyCount={timer.buyCount} stopCount={timer.holdCount} />
       </div>
     </div>
