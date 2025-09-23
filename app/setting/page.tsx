a79b21fa48291358c1ce3f7fae53cad8fa6a7083
relkimm <relkimm@grandeclip.com>
2025-09-23 21:23:03 +0900

    feat: 설정 화면 > 이용약관 & 개인정보처리 방침 링크 연결

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -18,14 +18,22 @@ const Page = () => {
     router.push('/setting/account-delete');
   };
 
+  const onTermsOfService = () => {
+    window.open('https://www.notion.so/230b5c7320fe8058b6c4e07aa2f2fcc3', '_blank');
+  };
+
+  const onPrivacyPolicy = () => {
+    window.open('https://www.notion.so/230b5c7320fe8015a782e58d174f594b', '_blank');
+  };
+
   return (
     <div className="h-dvh">
       <AppBar title="설정" leadings={<BackButton />} />
       <div className="px-4 flex flex-col items-start">
         <SettingItem label="로그아웃" onClick={onLogout} />
         <SettingItem label="회원 탈퇴" onClick={onAccountDelete} />
-        <SettingItem label="서비스 이용약관" onClick={() => {}} />
-        <SettingItem label="개인정보 처리방침" onClick={() => {}} />
+        <SettingItem label="서비스 이용약관" onClick={onTermsOfService} />
+        <SettingItem label="개인정보 처리방침" onClick={onPrivacyPolicy} />
         <SettingItem label="문의" onClick={() => {}} />
       </div>
     </div>
